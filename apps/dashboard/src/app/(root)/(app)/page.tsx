174593360c7c57fde5c1a43bb02d8eb76f976e46
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-10-08 09:51:48 +0530

    error solving

--- apps/dashboard/src/app/(root)/(app)/page.tsx
+++ apps/dashboard/src/app/(root)/(app)/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Button size="sm">Submit</Button>
         <p>{data.greeting}</p>
       </div>
     </div>
